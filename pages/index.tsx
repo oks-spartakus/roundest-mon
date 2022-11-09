0cfadaf7af60b01e34e792270fa1390ae72c235a
oks-spartakus <oks.spartakus@gmail.com>
2022-11-09 11:14:24 +0100

    basic ui structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemnon is rounder?</div>
+      <div className="p-2" />
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">VS</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
 }
